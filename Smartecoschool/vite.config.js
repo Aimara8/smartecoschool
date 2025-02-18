b1d82c6b0fdf16fe8211e93b2d5da984e544dd51
samuel <samueljimenezcontacto@gmail.com>
2025-02-18 10:32:32 +0000

    Resolución de merge

--- Smartecoschool/vite.config.js
+++ Smartecoschool/vite.config.js
@@ -5,13 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-<<<<<<< HEAD
     host: "0.0.0.0",
     port: 5173,  
   },  
-=======
-    host: "0.0.0.0", //Permiter acceso desde otros dispositivos
-    port: 5173,
-  }
->>>>>>> 917983ddf41fd0c23911de1c572656f36b97131a
 })
